90cd94636de89481c94eb744130397fc09d83cb4
Xizzo <wuytack.johnny@gmail.com>
2019-08-28 16:13:34 +0200

    Initial commit

---
+++ LeaveMeOn/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LeaveMeOn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lstLog
+            // 
+            this.lstLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLog.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 12);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(499, 225);
+            this.lstLog.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(401, 248);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(523, 283);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Leave Me On";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LeaveMeOn/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace LeaveMeOn
+{
+    public partial class Form1 : Form
+    {
+
+        List<WindowsInput.Native.VirtualKeyCode> ListKeys = new List<WindowsInput.Native.VirtualKeyCode>();
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddActions();
+        }
+
+        private void AddActions()
+        {
+            ListKeys.Add(WindowsInput.Native.VirtualKeyCode.VK_Z);
+            ListKeys.Add(WindowsInput.Native.VirtualKeyCode.VK_S);
+            ListKeys.Add(WindowsInput.Native.VirtualKeyCode.VK_D);    
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                timer1.Start();
+                btnStart.Text = "Stop";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lstLog.Items.Add($"[{DateTime.Now}] Insert Actions");
+            Application.DoEvents();
+
+            int i = random.Next(0, ListKeys.Count);
+            InputSimulator inputSim = new InputSimulator();
+            inputSim.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SPACE);
+            inputSim.Keyboard.KeyDown(ListKeys[i]);
+
+            System.Threading.Thread.Sleep(random.Next(500, 1500));
+
+            inputSim.Keyboard.KeyUp(ListKeys[i]);
+
+            timer1.Stop();
+            timer1.Interval = random.Next(3 * 60000, 4 * 60000);
+            timer1.Start();
+
+        }
+    }
+}
